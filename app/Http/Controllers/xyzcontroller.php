2447e9fd7da85ed6d3f7c0475af8935112fdd7c6
1395006.aptechiic@gmail.com <1395006.aptechiic@gmail.com>
2023-07-27 14:41:21 +0500

    this is my laravel controller files

---
+++ app/Http/Controllers/xyzcontroller.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class xyzcontroller extends Controller
+{
+    function Greeting(){
+        echo"<h1>HELLO EVERYONE</h1>";
+    }
+
+    function Greeting1(){
+        echo"<h1>HELLO EVERYONE</h1>";
+    }
+}
